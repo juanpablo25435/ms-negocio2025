f855819c8918ba99ea659f4892ff9a3fe46d5dc2
Juan Pablo <juan.1701713605@ucaldas.edu.co>
2025-04-29 18:12:10 -0500

    Se actualizan las rutas y se importan en routes.ts

--- start/routes/Municipalty.ts
+++
@@ -1,8 +0,0 @@
-import Route from '@ioc:Adonis/Core/Route'
-Route.group(() => {
-    Route.get("/municipalties", "MunicipaltiesController.find");
-    Route.get("/municipalties/:id", "MunicipaltiesController.find");
-    Route.post("/municipalties", "MunicipaltiesController.create");
-    Route.put("/municipalties/:id", "MunicipaltiesController.update");
-    Route.delete("/municipalties/:id", "MunicipaltiesController.delete");
-})
